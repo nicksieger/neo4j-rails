9c7c8a627e0cac6582a8b8a1e71ed415934b40b6
Nick Sieger <nick@nicksieger.com>
2010-04-09 09:51:36 -0400

    Don't assume that neo4j configuration exists

--- lib/neo4j/railtie.rb
+++ lib/neo4j/railtie.rb
@@ -4,7 +4,7 @@ require 'neo4j/transaction_management'
 module Neo4j
   class Railtie < Rails::Railtie
     initializer "neo4j.config" do |app|
-      Neo4j::Config.setup.merge!(app.config.neo4j.to_hash)
+      Neo4j::Config.setup.merge!(app.config.neo4j.to_hash) if app.config.respond_to?(:neo4j)
     end
 
     initializer "neo4j.tx" do |app|
