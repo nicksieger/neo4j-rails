22dec52d81e69681c2f1e42664a72d94ac0d23c4
Nick Sieger <nick@nicksieger.com>
2010-06-04 14:45:40 -0500

    Ensure neo4j config options hash is prepared in the railtie

--- lib/neo4j/railtie.rb
+++ lib/neo4j/railtie.rb
@@ -3,8 +3,10 @@ require 'neo4j/transaction_management'
 
 module Neo4j
   class Railtie < Rails::Railtie
+    config.neo4j = ActiveSupport::OrderedOptions.new
+
     initializer "neo4j.config" do |app|
-      Neo4j::Config.setup.merge!(app.config.neo4j.to_hash) if app.config.respond_to?(:neo4j)
+      Neo4j::Config.setup.merge!(app.config.neo4j.to_hash)
     end
 
     initializer "neo4j.tx" do |app|
