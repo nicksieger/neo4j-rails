493849b9ad6cfcb790482d2fd450ac8e98a546bd
Nick Sieger <nick@nicksieger.com>
2010-06-05 09:53:06 -0500

    Add 'lucene' config option to railtie

--- lib/neo4j/railtie.rb
+++ lib/neo4j/railtie.rb
@@ -4,6 +4,7 @@ require 'neo4j/transaction_management'
 module Neo4j
   class Railtie < Rails::Railtie
     config.neo4j = ActiveSupport::OrderedOptions.new
+    config.lucene = ActiveSupport::OrderedOptions.new
 
     initializer "neo4j.tx" do |app|
       app.config.middleware.use Neo4j::TransactionManagement
@@ -13,6 +14,7 @@ module Neo4j
     # register migrations in config/initializers
     initializer "neo4j.start", :after => :load_config_initializers do |app|
       Neo4j::Config.setup.merge!(app.config.neo4j.to_hash)
+      Lucene::Config.setup.merge!(app.config.lucene.to_hash)
       Neo4j.start
     end
   end
